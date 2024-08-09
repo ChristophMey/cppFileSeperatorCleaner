37ebadc4e228ae50771be96095e487b741a4a465
christoph meyer <christoph.meyer@kiongroup.com>
2024-08-09 14:42:07 +0200

    add README.md

--- file_cleaner/cleaner.py
+++ file_cleaner/cleaner.py
@@ -1,3 +1,7 @@
+"""
+This module contains the cleaner functions
+"""
+
 import os
 import logging
 
@@ -20,40 +24,66 @@ patterns = [
 logger = logging.getLogger(__name__)
 
 
-def deleteSeperatorsInFile(file):
-    with open(file, 'r') as f:
+def delete_seperator_lines(file: str):
+    """
+    Delete the separators in a file
+    :param file: the file to process
+    :return: None
+    """
+    with open(file, "r", encoding='utf-8') as f:
         lines = f.readlines()
 
-    with open(file, 'w') as f:
+    with open(file, 'w', encoding='utf-8') as f:
         for line in lines:
             if line.strip() not in patterns:
                 f.write(line)
 
 
-def findAllCxxAndHxxFilesInDir(dir):
-    logger.info("Finding all .cxx and .hxx files in directory: %s", dir)
+def find_all_cxx_and_hxx_files_in_dir(searched_directory: str):
+    """
+    Find all .cxx and .hxx files in a directory
+
+    :param searched_directory:
+    :return:
+    """
+    logger.info("Finding all .cxx and .hxx files in directory: %s", searched_directory)
     files = []
-    for root, dirs, fileNames in os.walk(dir):
+    for root, _, file_names in os.walk(searched_directory):
         if "3rd_party" in root:
             continue
-        for fileName in fileNames:
-            if fileName.endswith(".cxx") or fileName.endswith(".hxx") or fileName.endswith(".cpp") or fileName.endswith(".hpp"):
-                logger.info("Found file: %s", os.path.join(root, fileName))
-                files.append(os.path.join(root, fileName))
+
+        for file_name in file_names:
+            if (file_name.endswith(".cxx")
+                    or file_name.endswith(".hxx")
+                    or file_name.endswith(".cpp")
+                    or file_name.endswith(".hpp")):
+                logger.info("Found file: %s", os.path.join(root, file_name))
+                files.append(os.path.join(root, file_name))
     return files
 
 
 def init_argparse():
-    parser = argparse.ArgumentParser(description="Delete seperators in cxx and hxx files")
-    parser.add_argument("-d", "--dir", help="Directory to search for cxx and hxx files", required=True)
+    """
+    Initialize the argument parser
+    :return: the parsed arguments
+    """
+    parser = argparse.ArgumentParser(description="Delete separators in cxx and hxx files")
+    parser.add_argument("-d",
+                        "--dir",
+                        help="Directory to search for cxx and hxx files",
+                        required=True)
     return parser.parse_args()
 
 
 def main():
+    """
+    Main function
+    :return: None
+    """
     args = init_argparse()
     logger.setLevel(logging.INFO)
-    files = findAllCxxAndHxxFilesInDir(args.dir)
+    files = find_all_cxx_and_hxx_files_in_dir(args.dir)
     for file in files:
         logger.info("Processing file: %s", file)
-        deleteSeperatorsInFile(file)
+        delete_seperator_lines(file)
     logger.info("Done")
